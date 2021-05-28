1d5efb18b4c5fe76b22e514e9d8959a48e1a3702
Maria Serobabina <mariaserobabina@gmail.com>
2021-05-27 21:05:33 -0400

    adding some changes

---
+++ src/Day47_7_Encapsulation/AddressLIst.java
@@ -0,0 +1,27 @@
+package Day47_7_Encapsulation;
+
+public class AddressLIst {
+    public static void main(String[] args) {
+        Address cybertekAddress = new Address(); //calling the constructor automatically
+        cybertekAddress.setStreet("7925 Jones Branch Dr Suite 3300");
+        cybertekAddress.setCity("McLean");
+        cybertekAddress.setState("VA");
+        cybertekAddress.setZipCode("22182");
+
+        System.out.println("CybertekSchool address: " + cybertekAddress.toString());
+        cybertekAddress.setStreet("7925 Jones Branch Dr Suite 3200");
+
+        System.out.println("address after update = " + cybertekAddress);
+        System.out.println("street info = " + cybertekAddress.getStreet());
+
+        Address newAddress = new Address();
+        newAddress.setStreet("7921 Jones branch dr Suite 200");
+        newAddress.setCity("McLean");
+        newAddress.setState("VA");
+        newAddress.setZipCode("22102");
+        System.out.println(newAddress.toString());
+
+      //  Address papaJon = new Address("8501 Tyco Rd Ste 1A", "Vienna", "VA", "22182");
+        //BREAK TILL 9:11 PM EST
+    }
+}
--- src/Day_47_Encapsulation/AddressList.java
+++
@@ -1,7 +0,0 @@
-package Day_47_Encapsulation;
-
-public class AddressList {
-    public static void main(String[] args) {
-
-    }
-}
